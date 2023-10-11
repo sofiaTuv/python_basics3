31581fee1c35663135d5abe69473cb666b0d03cc
Sofia Tuvykina <hdfaa.56@gmail.com>
2023-10-11 20:24:08 +0300

    + models

---
+++ homework/models.py
@@ -0,0 +1,63 @@
+class Product:
+    name: str
+    price: float
+    description: str
+    quantity: int
+
+    def __init__(self, name, price, description, quantity):
+        self.name = name
+        self.price = price
+        self.description = description
+        self.quantity = quantity
+
+    def check_quantity(self, quantity) -> bool:
+        return self.quantity >= quantity
+
+    def buy(self, quantity):
+        if self.check_quantity(quantity):
+            self.quantity -= quantity
+        else:
+            raise ValueError
+
+    def __hash__(self):
+        return hash(self.name + self.description)
+
+
+class Cart:
+
+    products: dict[Product, int]
+
+    def __init__(self):
+        self.products = {}
+
+    def add_product(self, product: Product, buy_count=1):
+        if product in self.products:
+            self.products[product] += buy_count
+        else:
+            self.products[product] = buy_count
+
+    def remove_product(self, product: Product, remove_count=None):
+        if product in self.products or remove_count >= self.products[product]:
+            if remove_count is None:
+                del self.products[product]
+            else:
+                self.products[product] -= remove_count
+
+    def clear(self):
+        self.products.clear()
+
+    def get_total_price(self) -> float:
+        total_price = 0
+        for product, quantity in self.products.items():
+            total_price += product.price * quantity
+        return total_price
+
+    def buy(self):
+        if any(not product.check_quantity(quantity)
+               for product, quantity in self.products.items()):
+            raise ValueError('Недостаточно товаров')
+
+        for product, quantity in self.products.items():
+            product.buy(quantity)
+
+        self.clear()
